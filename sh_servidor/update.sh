54acb00e2a2d7f877195f2bf4e74c7d7d618d65b
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2018-06-04 22:58:31 -0300

    Update update.sh

--- sh_servidor/update.sh
+++ sh_servidor/update.sh
@@ -6,17 +6,17 @@
 echo "Iniciando a atualizacao"
 cd /usr/local/cpanel/3rdparty/csonic/
 rm -f menu.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/menu.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/menu.txt
 mv -f menu.txt menu.php
 cd /usr/local/cpanel/3rdparty/csonic/tools
 rm -f horacerta.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
 mv -f horacerta.txt horacerta.php
 rm -f programetes.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/programetes.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/programetes.txt
 mv -f programetes.txt programetes.php
 rm -f player_topo.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/player_topo.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/player_topo.txt
 mv -f player_topo.txt player_topo.php
 
 echo "Atualizacao concluida"
