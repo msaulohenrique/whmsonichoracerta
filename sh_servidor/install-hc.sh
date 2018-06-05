13ef4afb4abe2a22e44d2f3192048cbc8ebd9383
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2018-06-04 22:57:18 -0300

    Update install-hc.sh

--- sh_servidor/install-hc.sh
+++ sh_servidor/install-hc.sh
@@ -7,27 +7,27 @@ cd /usr/local/cpanel/3rdparty/csonic/
 
 echo "Alterando arquivo menu.php"
 rm -f menu.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/menu.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/menu.txt
 mv -f menu.txt menu.php
 
 echo "Criando imagem clock.png"
 cd /usr/local/cpanel/3rdparty/csonic/images
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/images/clock.png
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/images/player_topo.png
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/images/programetes.png
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/images/clock.png
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/images/player_topo.png
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/images/programetes.png
 
 echo "Instalando Hora Certa"
 cd /usr/local/cpanel/3rdparty/csonic/tools
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/xmlapi.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/xmlapi.txt
 mv -f xmlapi.txt xmlapi.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
 mv -f horacerta.txt horacerta.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/programetes.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/programetes.txt
 mv -f programetes.txt programetes.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/player_topo.txt
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/tools/player_topo.txt
 mv -f player_topo.txt player_topo.php
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/update.sh
-wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/remove.sh
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/update.sh
+wget https://msaulohenrique.github.io/whmsonichoracerta/sh_servidor/remove.sh
 chmod +x update.sh
 chmod +x remove.sh
 
